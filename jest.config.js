c5413f0f3168586f2f437363cc789c1b2fe29ac0
yoshi5534 <yoshi5534@users.noreply.github.com>
2025-03-23 23:11:13 +0100

    Update Jest and TypeScript configurations for ESM support and module resolution. Changed Jest config to use ES modules and updated TypeScript settings to target ESNext with improved module resolution and additional options.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+export default {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
+            useESM: true,
+        }],
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    extensionsToTreatAsEsm: ['.ts']
 }; 
